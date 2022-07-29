02cf35fbb5702b694604a71d9914828ae554f968
Bill Bejeck <bbejeck@gmail.com>
2022-07-29 17:18:25 -0400

    Initial commit to get TPL project going

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace TplKafka
+{
+    class TplKafkaStreaming
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TPL Kafka!");
+        }
+    }
+
+}
+
